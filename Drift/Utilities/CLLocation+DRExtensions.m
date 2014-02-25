a8ea20fded26588f601ea6b6966ae6d736c4eb1f
Christoph Albert <mail@christophalbert.com>
2014-02-25 20:22:40 +0100

    c function goodness

--- Drift/Utilities/CLLocation+DRExtensions.m
+++ Drift/Utilities/CLLocation+DRExtensions.m
@@ -8,7 +8,12 @@
 
 #import "CLLocation+DRExtensions.h"
 
-static const CGFloat r = 6378137.f;
+float const kEarthRadius = 6367500.f;
+
+float degreesToRadians(float angle) {
+    return ((angle) / 180.f * M_PI);
+}
+
 @import GLKit;
 
 @implementation CLLocation (DRExtensions)
@@ -34,7 +39,7 @@ static const CGFloat r = 6378137.f;
 
     CGFloat cosTOPooi = GLKVector3DotProduct(T, P)/GLKVector3Length(P);
     CGFloat TOPooi = acos(cosTOPooi);
-    CGFloat result = fabs((M_PI_2-TOPooi))*r;
+    CGFloat result = fabs((M_PI_2-TOPooi))*kEarthRadius;
 
     return result;
 }
@@ -43,38 +48,22 @@ static const CGFloat r = 6378137.f;
     CGFloat lat = self.coordinate.latitude;
     CGFloat lon = self.coordinate.longitude;
 
-    GLKVector3 vec = GLKVector3Make(r*cos([self longitudeTo2Pi:lon])*sin([self colatitude:lat]),
-                                     r*sin([self longitudeTo2Pi:lon])*sin([self colatitude:lat]),
-                                     r*cos([self colatitude:lat]));
+    GLKVector3 vec = GLKVector3Make(kEarthRadius*cos([self longitudeTo2Pi:lon])*sin([self colatitude:lat]),
+                                     kEarthRadius*sin([self longitudeTo2Pi:lon])*sin([self colatitude:lat]),
+                                     kEarthRadius*cos([self colatitude:lat]));
 
     return vec;
 }
 
 -(CGFloat)longitudeTo2Pi:(CGFloat)longitude {
     if (longitude < 0) {
-        return [self toRadians:360.f+longitude];
+        return degreesToRadians(360.f+longitude);
     }
-    return [self toRadians:longitude];
+    return degreesToRadians(longitude);
 }
 
 -(CGFloat)colatitude:(CGFloat)latitude {
-    return [self toRadians:90.f-latitude];
-}
-
--(CGFloat)toRadians:(CGFloat)degrees {
-    return degrees*M_PI/180.f;
-}
-
-- (NSDecimalNumber *)absoluteValueOfDecimalNumber:(NSDecimalNumber *)num {
-    if ([num compare:[NSDecimalNumber zero]] == NSOrderedAscending) {
-        // Number is negative. Multiply by -1
-        NSDecimalNumber * negativeOne = [NSDecimalNumber decimalNumberWithMantissa:1
-                                                                          exponent:0
-                                                                        isNegative:YES];
-        return [num decimalNumberByMultiplyingBy:negativeOne];
-    } else {
-        return num;
-    }
+    return degreesToRadians(90.f-latitude);
 }
 
 //-(CLLocation *)dr_perpendicularLocationWithLocation:(CLLocation *)firstLocation location:(CLLocation *)secondLocation {
