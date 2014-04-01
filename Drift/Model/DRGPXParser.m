5fadadf22905e91f6d1cc6476acf8e1cf0e03b81
Christoph Albert <mail@christophalbert.com>
2014-04-01 16:12:44 +0200

    gpx register and message

--- Drift/Model/DRGPXParser.m
+++ Drift/Model/DRGPXParser.m
@@ -10,6 +10,7 @@
 #import "TBXML+NSDictionary.h"
 #import "DRModel.h"
 #import "GPX.h"
+#import "SIAlertView.h"
 
 @implementation DRGPXParser
 
@@ -20,7 +21,29 @@
     
     GPXRoot *root = [GPXParser parseGPXAtURL:url];
 
-    for (GPXRoute *route in root.routes) {
+    if ([root.routes count] == 0) {
+        SIAlertView *alert = [[SIAlertView alloc] initWithTitle:nil andMessage:NSLocalizedString(@"No routes found in GPX file.", nil)];
+        [alert addButtonWithTitle:NSLocalizedString(@"Okay :(", nil) type:SIAlertViewButtonTypeDefault handler:nil];
+        [alert show];
+    } else if ([root.routes count] > 0) {
+        NSString *message;
+        if ([root.routes count] == 1) {
+            message = NSLocalizedString(@"Do you want to import one route?", nil);
+        } else {
+            message = [NSString stringWithFormat:NSLocalizedString(@"Do you want to import %li routes?", nil),[root.routes count]];
+        }
+
+        SIAlertView *alert = [[SIAlertView alloc] initWithTitle:nil andMessage:message];
+        [alert addButtonWithTitle:NSLocalizedString(@"Cancel", nil) type:SIAlertViewButtonTypeCancel handler:nil];
+        [alert addButtonWithTitle:NSLocalizedString(@"Import", nil) type:SIAlertViewButtonTypeDefault handler:^(SIAlertView *alertView) {
+            [DRGPXParser importAndSaveRoutes:root.routes];
+        }];
+        [alert show];
+    }
+}
+
++(void)importAndSaveRoutes:(NSArray *)routes {
+    for (GPXRoute *route in routes) {
         NSMutableArray *locations = [[NSMutableArray alloc] init];
         for (GPXRoutePoint *point in route.routepoints) {
             CLLocation *loc = [[CLLocation alloc] initWithLatitude:point.latitude longitude:point.longitude];
@@ -28,12 +51,11 @@
                 [locations addObject:loc];
             }
         }
-        if ([locations count] > 0) {
-            NSManagedObjectContext *context = [NSManagedObjectContext MR_context];
-            DRPath *path = [DRPath MR_createInContext:context];
-            path.locations = locations;
-            [context MR_saveToPersistentStoreAndWait];
-        }
+
+        NSManagedObjectContext *context = [NSManagedObjectContext MR_context];
+        DRPath *path = [DRPath MR_createInContext:context];
+        path.locations = locations;
+        [context MR_saveToPersistentStoreAndWait];
     }
 }
 
